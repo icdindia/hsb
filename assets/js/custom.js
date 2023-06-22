f40755c33376bc804f320becadd4233216d03979
shivani <shivani@icdindia.com>
2023-06-22 18:16:16 +0530

    protoype

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,11 @@
-// var btnToggle = document.getElementsByClassName("hamburger");
-// btnToggle[0].addEventListener("click", function () {
-// 	$(this).toggleClass('open');
-// 	$('.menu-header-container').toggleClass('d-sm-block');
-// 	//  $('.menu li:nth-child(1)').toggleClass('o-show');
-// });
-
 
 
 $('#fullpage').fullpage({
 	sectionSelector: '.section',
-	// scrollBar: true,
 	normalScrollElements: '.scrolldiv',
-	// scrollOverflow: true,
-	// offsetSections: true,
 	scrollingSpeed: 800,
 	easing: 'easeOut',
+	  licenseKey: 'S6K7J-034TK-Z98KK-J06DH-OOKRQ'
 }
 );
 
